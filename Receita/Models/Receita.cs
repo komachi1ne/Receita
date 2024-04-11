7651608d835af1c3113b023befc6002a33215b22
komachi1ne <isabellevasconcelos862@gmail.com>
2024-04-11 12:28:33 -0300

    pag 84, dúvidas no Tipo.cs

---
+++ Receita/Models/Receita.cs
@@ -0,0 +1,11 @@
+namespace Receita.Models;
+
+    public class Receitas
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public List<string> Tipo { get; set; } = [];
+        public double Preparo { get; set; }
+        public string Imagem { get; set; }
+    }
---
+++ Receita/Models/Tipo.cs
@@ -0,0 +1,7 @@
+namespace Receita.Models
+
+    public class Tipo
+    {
+        public string Nome { get; set; }
+        public string  { get; set; }
+    }
